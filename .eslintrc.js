632300639816d40f0c07da2224dbbdaf0275f7ff
Marcelo Meireles <marcelomeireles@MBP-de-Marcelo.lan>
2019-11-06 21:46:18 +0000

    preetier installed

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,8 +4,10 @@ module.exports = {
         "node": true
     },
     "extends": [
-        "airbnb-base"
+        "airbnb-base",
+        "prettier"
     ],
+    "plugins": ["prettier"],
     "globals": {
         "Atomics": "readonly",
         "SharedArrayBuffer": "readonly"
@@ -15,6 +17,7 @@ module.exports = {
         "sourceType": "module"
     },
     "rules": {
+        "prettier/prettier": "error",
         "class-methods-use-this": "off",
         "no-param-reassign": "off",
         "camelcase": "off",
