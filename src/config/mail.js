25e1dc05d7aa3353cbeab2611565df76d64bf2a1
Marcelo Meireles <marcelomeireles@MBP-de-Marcelo.lan>
2019-11-29 12:46:11 +0000

    Cancel appointment email method

---
+++ src/config/mail.js
@@ -0,0 +1,18 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '2bd85e4fc1e537',
+    pass: 'c293e51d12afc8',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
+
+// Amazon sessionStorageMailgun
+// Sparkpost
+// Madril (Mailchimp)
+// Gmails (SMTP limit)
+// Mailtrap (--dev)
