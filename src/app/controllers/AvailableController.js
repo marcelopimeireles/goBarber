e446d36047e332fe153c440b2f9c7cc28442fbc8
Marcelo Meireles <marcelomeireles@MBP-de-Marcelo.lan>
2019-12-22 21:40:52 +0000

    list of available hours by provider - route and empty controller

---
+++ src/app/controllers/AvailableController.js
@@ -0,0 +1,18 @@
+// import * as Yup from 'yup';
+// import { startOfHour, parseISO, isBefore, format, subHours } from 'date-fns';
+// import pt from 'date-fns/locale/pt';
+// import User from '../models/User';
+// import File from '../models/File';
+// import Appointment from '../models/Appointment';
+// import Notification from '../schemas/Notification';
+
+// import CancellationMail from '../jobs/CancellationMail';
+// import Queue from '../../lib/Queue';
+
+class AvailableController {
+  async index(req, res) {
+    return res.json();
+  }
+}
+
+export default new AvailableController();
